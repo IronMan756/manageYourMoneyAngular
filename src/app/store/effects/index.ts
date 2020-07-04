4676b598350a53ea123180d05b7b1614759708b7
IronMan756 <andrewstarudubtsev@gmail.com>
2020-07-04 18:20:04 +0300

    hotfix

--- src/app/store/effects/index.ts
+++ src/app/store/effects/index.ts
@@ -2,6 +2,7 @@ import { AuthEffects } from './auth.effect';
 import { ExpencesEffects } from './expences.effects';
 import { IncomesEffects } from './incomes.effect';
 
+
 export const effects = [
     AuthEffects,
     ExpencesEffects,
